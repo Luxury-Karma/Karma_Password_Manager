c949cafa6f3ecb8ee0c4fc4f03e0d2cd3385b476
Luxury-Karma <108990098+Luxury-Karma@users.noreply.github.com>
2024-03-14 15:36:55 -0400

    base flask is setup

--- Modual/webserver_modual.py
+++ Modual/webserver_modual.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
-app = Flask(__name__)
+from flask import Flask, request, render_template
+app = Flask(__name__, template_folder='../HTML')
+
 
 @app.route('/update_password', methods=['POST'])
 def update_password():
@@ -8,6 +9,11 @@ def update_password():
     website = request.form.get('website')
     # Call your Python script here, or move its functionality into this script
     return "Password updated successfully"
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', name='Home Page')
 
 
 def run_web_server():
